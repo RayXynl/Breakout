7d44decb4686b93a8197f31f7e4ddf158d2a3582
RayXynl <98826395+RayXynl@users.noreply.github.com>
2024-10-25 13:10:37 +0100

    Ball Collision Functions

--- Breakout/Ball.cpp
+++ Breakout/Ball.cpp
@@ -4,7 +4,7 @@
 
 Ball::Ball(sf::RenderWindow* window, float velocity, float radius, GameManager* gameManager)
     : _window(window), _velocity(velocity), _radius(radius), _gameManager(gameManager),
-    _timeWithPowerupEffect(0.f), _isFireBall(false), _isAlive(true), _direction({1,1})
+    _timeWithPowerupEffect(0.f), _isFireBall(false), _isAlive(true), _direction({ 1,1 })
 {
     _sprite.setRadius(RADIUS);
     _sprite.setFillColor(sf::Color::Cyan);
@@ -48,21 +48,10 @@ void Ball::update(float dt)
         {
             setFireBall(0);    // disable fireball
             _sprite.setFillColor(sf::Color::Cyan);  // back to normal colour.
-        }        
+        }
     }
 
-
-    // Fireball effect
-    if (_isFireBall)
-    {
-        // Flickering effect
-        int flicker = rand() % 50 + 205; // Random value between 205 and 255
-        _sprite.setFillColor(sf::Color(flicker, flicker / 2, 0)); // Orange flickering color
-    }
-
-    // Update position with a subtle floating-point error
-    _sprite.move(_direction * _velocity * dt);
-
+    // Ball Trail //
     if (!_ballTrail.empty())
         _ballTrail.front().setPosition(_sprite.getPosition());
 
@@ -73,24 +62,30 @@ void Ball::update(float dt)
 
         _ballTrail[i].move((lastBallPos - currentBallPos) * (_velocity * 0.07f) * dt);
     }
+    // ^Ball Trail ^//
+
+    // Fireball effect
+    if (_isFireBall)
+    {
+        // Flickering effect
+        int flicker = rand() % 50 + 205; // Random value between 205 and 255
+        _sprite.setFillColor(sf::Color(flicker, flicker / 2, 0)); // Orange flickering color
+    }
+
+
+
+
+    // Update position with a subtle floating-point error
+    _sprite.move(_direction * _velocity * dt);
+
 
     // check bounds and bounce
     sf::Vector2f position = _sprite.getPosition();
-    sf::Vector2u windowDimensions = _window->getSize();
+    boundaryCollision(position, _direction);
 
-    // bounce on walls
-    if ((position.x >= windowDimensions.x - 2 * _radius && _direction.x > 0) || (position.x <= 0 && _direction.x < 0))
-    {
-        _direction.x *= -1;
-    }
-
-    // bounce on ceiling
-    if (position.y <= 0 && _direction.y < 0)
-    {
-        _direction.y *= -1;
-    }
 
     // lose life bounce
+    sf::Vector2u windowDimensions = _window->getSize();
     if (position.y > windowDimensions.y)
     {
         _sprite.setPosition(0, 300);
@@ -101,35 +96,17 @@ void Ball::update(float dt)
     // collision with paddle
     if (_sprite.getGlobalBounds().intersects(_gameManager->getPaddle()->getBounds()))
     {
-        _gameManager->getSoundManager()->playSound(paddleBounce); // Play paddle bounce sound
-        _direction.y *= -1; // Bounce vertically
-        float paddlePositionProportion = (_sprite.getPosition().x - _gameManager->getPaddle()->getBounds().left) / _gameManager->getPaddle()->getBounds().width;
-        _direction.x = paddlePositionProportion * 2.0f - 1.0f;
-
-        // Adjust position to avoid getting stuck inside the paddle
-        _sprite.setPosition(_sprite.getPosition().x, _gameManager->getPaddle()->getBounds().top - 2 * _radius);
+        paddleCollision(_sprite, _direction);
     }
 
-    // collision with bricks
-    int collisionResponse = _gameManager->getBrickManager()->checkCollision(_sprite, _direction);
-    if (_isFireBall) return; // no collisisons when in fireBall mode.
-    if (collisionResponse == 1)
-    {
-        _direction.x *= -1; // Bounce horizontally
-     
-    }
-    else if (collisionResponse == 2)
-    {
-        _direction.y *= -1; // Bounce vertically
-     
-    }
+    brickCollision(_sprite, _direction);
 }
 
 void Ball::render()
 {
     for (int i = 0; i < _ballTrail.size(); i++)
     {
-        _window->draw(_ballTrail[i]); 
+        _window->draw(_ballTrail[i]);
     }
     _window->draw(_sprite);
 }
@@ -146,18 +123,68 @@ void Ball::setRadius(float radius, float duration)
     _timeWithPowerupEffect = duration;
     _sprite.setRadius(_radius);
 
-    for (auto& trails :_ballTrail)
+    for (auto& trails : _ballTrail)
         trails.setRadius(_radius);
-    
+
 }
 void Ball::setFireBall(float duration)
 {
-    if (duration) 
+    if (duration)
     {
         _isFireBall = true;
-        _timeWithPowerupEffect = duration;        
+        _timeWithPowerupEffect = duration;
         return;
     }
     _isFireBall = false;
-    _timeWithPowerupEffect = 0.f;    
+    _timeWithPowerupEffect = 0.f;
+}
+
+
+
+void Ball::paddleCollision(sf::CircleShape& sprite, sf::Vector2f& direction)
+{
+    // collision with paddle
+    if (sprite.getGlobalBounds().intersects(_gameManager->getPaddle()->getBounds()))
+    {
+        _gameManager->getSoundManager()->playSound(paddleBounce); // Play paddle bounce sound
+        direction.y *= -1; // Bounce vertically
+        float paddlePositionProportion = (sprite.getPosition().x - _gameManager->getPaddle()->getBounds().left) / _gameManager->getPaddle()->getBounds().width;
+        direction.x = paddlePositionProportion * 2.0f - 1.0f;
+
+        // Adjust position to avoid getting stuck inside the paddle
+        sprite.setPosition(sprite.getPosition().x, _gameManager->getPaddle()->getBounds().top - 2 * _radius);
+    }
+}
+
+void Ball::boundaryCollision(sf::Vector2f position, sf::Vector2f& direction)
+{
+    sf::Vector2u windowDimensions = _window->getSize();
+    // bounce on walls
+    if ((position.x >= windowDimensions.x - 2 * _radius && direction.x > 0) || (position.x <= 0 && direction.x < 0))
+    {
+        direction.x *= -1;
+    }
+
+    // bounce on ceiling
+    if (position.y <= 0 && direction.y < 0)
+    {
+        direction.y *= -1;
+    }
+
+}
+
+void Ball::brickCollision(sf::CircleShape sprite, sf::Vector2f& direction)
+{
+    // collision with bricks
+    int collisionResponse = _gameManager->getBrickManager()->checkCollision(sprite, direction);
+    if (_isFireBall) return; // no collisisons when in fireBall mode.
+    if (collisionResponse == 1)
+    {
+        direction.x *= -1; // Bounce horizontally
+
+    }
+    else if (collisionResponse == 2)
+    {
+        direction.y *= -1; // Bounce vertically
+    }
 }
--- Breakout/Ball.h
+++ Breakout/Ball.h
@@ -15,6 +15,11 @@ public:
     void setVelocity(float coeff, float duration);
     void setFireBall(float duration);
     void setRadius(float radius, float duration);
+
+    void paddleCollision(sf::CircleShape& sprite, sf::Vector2f& direction);
+    void boundaryCollision(sf::Vector2f position, sf::Vector2f& direction);
+    void brickCollision(sf::CircleShape sprite, sf::Vector2f& direction);
+
 private:
     sf::CircleShape _sprite;
     sf::Vector2f _direction;
@@ -28,8 +33,8 @@ private:
     GameManager* _gameManager;  // Reference to the GameManager
 
     std::vector<sf::CircleShape> _ballTrail;
-
-    static constexpr float RADIUS = 10.0f;      
+  
+    static constexpr float RADIUS = 10.0f;
     static constexpr float VELOCITY = 350.0f;   // for reference.
 };
 
